18a6df5c00bb7feb4de6cbc50e3970a5e2dd8afe
HansAmPfaall <EdgarJustus94@gmail.com>
2019-01-28 10:48:09 +0100

    Add files via upload

--- MazeGens/sketch.js
+++ MazeGens/sketch.js
@@ -1,13 +1,15 @@
-let swidth = screen.width - screen.width * 0.015
-let sheight = screen.height - screen.height * 0.15
+// let swidth = screen.width - screen.width * 0.015
+// let sheight = screen.height - screen.height * 0.15
+let swidth = 800 - 800 * 0.015
+let sheight = 600 - 600 * 0.15
 let goal = 0
 let pop;
 let obstacles = []
 
 function setup() {
-  obstacles.push(new Obstacle(swidth / 2, sheight / 2, 50, 400))
-  obstacles.push(new Obstacle(swidth / 4, 0, 50, 400))
-  obstacles.push(new Obstacle(swidth / 4 + swidth / 2, 0, 50, 400))
+  obstacles.push(new Obstacle(swidth / 2, sheight / 2, sheight / 10, sheight))
+  obstacles.push(new Obstacle(swidth / 2 - swidth / 4, -10, sheight / 10, sheight / 2))
+  obstacles.push(new Obstacle(swidth / 2 + swidth / 4, -10, sheight / 10, sheight / 2))
   pop = new Population()
   createCanvas(swidth, sheight);
   goal = createVector(swidth / 2 + swidth / 4 + swidth / 5, sheight / 2)
@@ -18,6 +20,7 @@ function setup() {
 function draw() {
   background(255);
   noFill();
+  rect(0, 0, swidth - 1, sheight - 1)
   stroke(75)
   ellipse(goal.x, goal.y, 50, 50);
   for (var i = 0; i < obstacles.length; i++) {
