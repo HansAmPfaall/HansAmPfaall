3ff8264e9490c41484c944eec2539dc6d0eef529
HansAmPfaall <EdgarJustus94@gmail.com>
2019-01-28 10:48:22 +0100

    Add files via upload

---
+++ Population.js
@@ -0,0 +1,72 @@
+function Population() {
+  this.ships = []
+  this.fitnessSum = 0
+
+
+  for (var i = 0; i < 500; i++) {
+    this.ships.push(new Ship())
+  }
+
+  this.update = function() {
+    for (var i = 0; i < 500; i++) {
+      this.ships[i].draw()
+    }
+    for (var i = 0; i < this.ships.length; i++) {
+      if (this.ships[i].dead == false) {
+        this.ships[i].move();
+      }
+    }
+  }
+
+  this.naturalSelection = function() {
+    this.fitnessSum = 0
+    newShips = []
+    this.calcAllFitness()
+    this.calcFitnessSum()
+    for (var i = 0; i < this.ships.length; i++) {
+      newShips.push(this.selectParent().duplicate())
+    }
+    this.ships = newShips
+  }
+
+  this.selectParent = function() {
+    let rand = random(this.fitnessSum)
+    let runningSum = 0
+    for (var i = 0; i < this.ships.length; i++) {
+      runningSum += this.ships[i].fitness
+      if (runningSum > rand) {
+        return this.ships[i]
+      }
+    }
+  }
+
+  this.allDead = function() {
+    for (var i = 0; i < this.ships.length; i++) {
+      if (this.ships[i].dead == false) {
+        return false
+      }
+    }
+    return true
+  }
+
+  this.calcAllFitness = function() {
+    for (var i = 0; i < this.ships.length; i++) {
+      this.ships[i].calcFitness()
+    }
+  }
+
+  this.calcFitnessSum = function() {
+    for (var i = 0; i < this.ships.length; i++) {
+      this.fitnessSum += this.ships[i].fitness
+    }
+  }
+
+  this.mutateAll = function() {
+    for (var i = 0; i < this.ships.length; i++) {
+      this.ships[i].dna.mutate()
+    }
+  }
+
+
+
+}
